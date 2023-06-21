48e63aa6b2cec31f035f50ab2a3a4a0827cff62c
Matthieu Dorier <mdorier@anl.gov>
2023-06-21 09:55:39 -0500

    removed unnecessary files

--- sloc.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-CLIENT=(include/poesie-client.h include/poesie-common.h src/poesie-client.c)
-SERVER=(include/poesie-server.h src/poesie-rpc-types.h src/poesie-server.c src/poesie-vm* src/lang/*)
-echo "************** CLIENT ***************"
-sloccount "${CLIENT[@]}"
-
-echo "************** SERVER ***************"
-sloccount "${SERVER[@]}"
