fcecde6e8d3bb7db2b390a95b9a4be853bd9e187
Matthieu Dorier <mdorier@anl.gov>
2024-09-19 10:31:53 +0100

    updated bedrock module to latest API (remains to be tested)

--- src/BedrockModule.cpp
+++ src/BedrockModule.cpp
@@ -6,70 +6,57 @@
 #include "poesie/Client.hpp"
 #include "poesie/Provider.hpp"
 #include "poesie/ProviderHandle.hpp"
-#include <bedrock/AbstractServiceFactory.hpp>
+
+#include <bedrock/AbstractComponent.hpp>
 
 namespace tl = thallium;
 
-class PoesieFactory : public bedrock::AbstractServiceFactory {
+class PoesieComponent : public bedrock::AbstractComponent {
+
+    std::unique_ptr<poesie::Provider> m_provider;
 
     public:
 
-    PoesieFactory() {}
+    PoesieComponent(const tl::engine& engine,
+                    uint16_t  provider_id,
+                    const std::string& config,
+                    const tl::pool& pool)
+    : m_provider{std::make_unique<poesie::Provider>(engine, provider_id, config, pool)}
+    {}
 
-    void *registerProvider(const bedrock::FactoryArgs &args) override {
-        auto provider = new poesie::Provider(args.mid, args.provider_id,
-                args.config, tl::pool(args.pool));
-        return static_cast<void *>(provider);
+    void* getHandle() override {
+        return static_cast<void*>(m_provider.get());
     }
 
-    void deregisterProvider(void *p) override {
-        auto provider = static_cast<poesie::Provider *>(p);
-        delete provider;
+    std::string getConfig() override {
+        return m_provider->getConfig();
     }
 
-    std::string getProviderConfig(void *p) override {
-        auto provider = static_cast<poesie::Provider *>(p);
-        return provider->getConfig();
-    }
+    static std::shared_ptr<bedrock::AbstractComponent>
+        Register(const bedrock::ComponentArgs& args) {
+            tl::pool pool;
+            auto it = args.dependencies.find("pool");
+            if(it != args.dependencies.end() && !it->second.empty()) {
+                pool = it->second[0]->getHandle<tl::pool>();
+            }
+            return std::make_shared<PoesieComponent>(
+                args.engine, args.provider_id, args.config, pool);
+        }
 
-    void *initClient(const bedrock::FactoryArgs& args) override {
-        return static_cast<void *>(new poesie::Client(args.mid));
-    }
-
-    void finalizeClient(void *client) override {
-        delete static_cast<poesie::Client *>(client);
-    }
-
-    std::string getClientConfig(void* c) override {
-        auto client = static_cast<poesie::Client*>(c);
-        return client->getConfig();
-    }
-
-    void *createProviderHandle(void *c, hg_addr_t address,
-            uint16_t provider_id) override {
-        auto client = static_cast<poesie::Client *>(c);
-        auto ph = new poesie::ProviderHandle(
-                client->engine(),
-                address,
-                provider_id,
-                false);
-        return static_cast<void *>(ph);
-    }
-
-    void destroyProviderHandle(void *providerHandle) override {
-        auto ph = static_cast<poesie::ProviderHandle *>(providerHandle);
-        delete ph;
-    }
-
-    const std::vector<bedrock::Dependency> &getProviderDependencies() override {
-        static const std::vector<bedrock::Dependency> no_dependency;
-        return no_dependency;
-    }
-
-    const std::vector<bedrock::Dependency> &getClientDependencies() override {
-        static const std::vector<bedrock::Dependency> no_dependency;
-        return no_dependency;
-    }
+    static std::vector<bedrock::Dependency>
+        GetDependencies(const bedrock::ComponentArgs& args) {
+            (void)args;
+            std::vector<bedrock::Dependency> dependencies{
+                bedrock::Dependency{
+                    /* name */ "pool",
+                    /* type */ "pool",
+                    /* is_required */ false,
+                    /* is_array */ false,
+                    /* is_updatable */ false
+                }
+            };
+            return dependencies;
+        }
 };
 
-BEDROCK_REGISTER_MODULE_FACTORY(poesie, PoesieFactory)
+BEDROCK_REGISTER_COMPONENT_TYPE(poesie, PoesieComponent)
